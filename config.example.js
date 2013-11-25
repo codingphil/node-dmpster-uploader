e2f57bab0b63bdb2ae11b472e69ab512e4bdca8c
codingphil <philip.lonsing@gmx.at>
2013-11-25 12:10:59 +0100

    Renamed config.js to config.example.js and added config.js to .gitignore

---
+++ config.example.js
@@ -0,0 +1,29 @@
+
+config = module.exports = { };
+
+config.host = 'localhost';
+//config.host = 'lnz-spdumpster';
+
+config.baseUrl = "http://" + config.host;
+config.uploadUrl = config.baseUrl + '/upload';
+
+config.maxParallelUploads = 2;
+
+config.autotagger = [
+    "osversion-tagger",
+    "machinename-tagger",
+    {
+      name: "envvar-tagger",
+      varName: "USERNAME",
+      formatter: function(varValue) { return "USER:" + varValue; }
+    }
+  ];
+
+config.postupload = [
+    //"postupload-delete"
+    {
+      name: "postupload-createhtmlfile",
+      template: undefined, // default. You can specify a custom EJS file here.
+      baseUrl: config.baseUrl
+    }
+  ];
